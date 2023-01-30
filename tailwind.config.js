24160f3a39aa287d00c3827391781ed8ed127d47
ebukanas <43076966+ebukanas@users.noreply.github.com>
2023-01-30 18:09:00 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./src/**/*.{js,jsx,ts,tsx}", 
+  "./src/components/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'purple': '#392c42',
+        'blue': '#0058ff',
+        'yellow' : '#fccf6d',
+        'pamaskomu-blue' : '#b0d1e4',
+        'gray' : '#767676',
+      },
+      height: {
+        '550': '550px',
+        '350': '350px',
+      },
+      backgroundSize: {
+        '550': '550px',
+        '350': '350px',
+        '700': '700px',
+        '250': '250px',
+      },
+      fontFamily: {
+        'futura': ['futura', 'bold'],
+      },
+      backgroundImage: {
+        'logoBlack': "url('/src/images/logo.png')",
+        'logoWhite': "url('/src/images/logo_white.png')",
+        'emailBlack': "url('/src/images/email.png')",
+        'phoneBlack': "url('/src/images/phone-call.png')",
+        'hero2': "url('/src/images/statue2.png')",
+      },
+    },
+  },
+  plugins: [],
+}
